2128f10f1c34f03fbb04eadd6a3e375b38e2fd75
1119879311 <1119879311@qq.com>
2022-03-08 23:46:36 +0800

    添加add 安装模块子命令

--- packages/cli/src/commands/add.command.js
+++ packages/cli/src/commands/add.command.js
@@ -1,5 +1,5 @@
 const AbstractCommand = require("./abstract.command");
-
+const crossSpawn = require("cross-spawn");
 // eslint-disable-next-line import/prefer-default-export
 module.exports = class AddCommand extends AbstractCommand {
   // eslint-disable-next-line class-methods-use-this
@@ -7,9 +7,19 @@ module.exports = class AddCommand extends AbstractCommand {
     program
       .command("add")
       .argument("<pkgName...>", "安装一个或者多个依赖包")
-      .option("-s,--save [mode]", "安装在哪个环境")
+      .option("-D,--dev [dev]", "安装在devDependencies")
+      .option("-P,--prod [prod]", "安装在dependencies个环境")
       .action((data, options) => {
-        console.log("action", this.action.handler(data, program.opts()));
+        console.log("options:", options, program.opts());
+        let { dev, prod } = options;
+        crossSpawn.sync(
+          "npm i",
+          [...data, dev && "--save-dev", prod && "--production"].filter(
+            Boolean
+          ),
+          { stdio: "inherit" }
+        );
+        console.log("process.execPath", process.execPath);
         console.log("run data:", data);
       });
   }
--- packages/cli/src/commands/index.js
+++ packages/cli/src/commands/index.js
@@ -5,8 +5,6 @@ const { AddAction, RunAction } = require("../actions");
 const loadCommand = (program) => {
   new AddCommand(AddAction.create()).load(program);
   new RunCommand(RunAction.create()).load(program);
-
-  // const options = program.opts();
 };
 
 exports.loadCommand = loadCommand;
